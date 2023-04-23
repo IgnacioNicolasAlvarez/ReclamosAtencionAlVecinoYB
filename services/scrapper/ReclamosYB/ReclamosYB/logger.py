ee03371e5c3eeca9ee9764be75c088bdde08ab0f
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2023-04-23 20:33:20 -0300

    add format

--- services/scrapper/ReclamosYB/ReclamosYB/logger.py
+++ services/scrapper/ReclamosYB/ReclamosYB/logger.py
@@ -1,14 +1,16 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+log_file = "reclamosyb.log"
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-handler = RotatingFileHandler("reclamosyb.log", maxBytes=100000, backupCount=1)
+handler = RotatingFileHandler(log_file, maxBytes=100000, backupCount=1)
 handler.setLevel(logging.INFO)
 
-
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(log_format)
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
