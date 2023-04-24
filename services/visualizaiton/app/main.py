51c1a0993b07d24cb2fcaf3df66b1fb3be1a53ef
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2023-04-24 10:12:26 -0300

    add to gitignore

--- services/visualizaiton/app/main.py
+++ services/visualizaiton/app/main.py
@@ -1,5 +1,28 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
-st.title("Uber pickups in NYC")
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+
+rows = run_query("SELECT * FROM `project-reclamos-yb.dataset_yb.Perdida de Agua` LIMIT 10")
+
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row["Municipio"])
